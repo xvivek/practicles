08d257a0a099344a8d8514f14268eadd8eb8bc30
xvivek <vivek_c81@yahoo.co.in>
2023-06-21 16:31:30 +0530

    tor impl
    https://sukesh.me/2021/08/22/how-to-browse-darkweb-using-net-6/

---
+++ csharp/Interview2023/Torify/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace Torify
+{
+    internal class Tor
+    {
+        private static async Task Main(string[] argv)
+        {
+            if (argv is null)
+            {
+                throw new ArgumentNullException(nameof(argv));
+            }
+
+            Uri url = new("https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion");
+
+            HttpClient client = new(new HttpClientHandler
+            {
+                // Magic happens with socks scheme used below
+                Proxy = new WebProxy("socks5://127.0.0.1:9050")
+            });
+            Console.WriteLine(await client.GetStringAsync(url));
+        }
+    }
+
+}
