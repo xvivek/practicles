fdbe66204e692aff8df4ebf4e0b6e627a960ef1e
xvivek <vivek_c81@yahoo.co.in>
2023-07-06 08:35:36 +0530

    chartGTP

---
+++ csharp/Interview2023/ProCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Calc c = new Calc();
+int ans = c.Add(10, 84);
+Console.WriteLine("10 + 84 is {0}.", ans);
+//Wait for user to press the Enter key
+Console.ReadLine();
+// The C# calculator.
+public class Calc
+{
+    public int Add(int addend1, int addend2)
+    {
+        return addend1 + addend2;
+    }
+}
