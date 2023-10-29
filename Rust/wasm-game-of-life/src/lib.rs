4f27052d160f6572a930a936cdeaebe59e1d69ac
xvivek <vivek_c81@yahoo.co.in>
2023-10-29 14:28:56 +0530

    rust notes

--- Rust/wasm-game-of-life/src/lib.rs
+++ Rust/wasm-game-of-life/src/lib.rs
@@ -12,6 +12,8 @@ pub fn greet() {
     alert("Hello, wasm-game-of-life!");
 }
 
+//https://hackernoon.com/building-a-simple-web-app-with-rust-and-webassembly-a-step-by-step-guide?source=rss
+//https://rustwasm.github.io/book/game-of-life/hello-world.html
 #[wasm_bindgen]
 pub extern "C" fn factorial(n: u32) -> u32 {
     if n == 0 {
